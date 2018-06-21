8ec9be607f6e8f3e3fe7dfa657e70a8ff8795b15
taylormuckle1 <danielleetaylorr1@gmail.com>
2018-06-21 20:01:19 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,18 @@
 #Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher, :activity
+  def initalize(detention, location="Room 301", time="3:00 PM", grumpy_teacher="John Schmidt", activity= "collective punishment")
+    @detention= detention
+    @location= location
+    @time= time
+    @grumpy_teacher= grumpy_teacher
+    @activity= activity
+  end
+end
+# dentention.new(:activity=" collective punishment")
+test=Detention.new
+
+puts test.location
+puts test.time
+puts test.grumpy_teacher
+puts test.activity
